0f6f522e826c470ad0363db0f749d7e46ee8a2e9
方忠朋 <992565438@qq.com>
2025-12-10 15:46:22 +0800

    备份角色用户菜单

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-tar zvcf bigdevops_`date +%Y-%m-%d_%H-%M-%S`.tar.gz --exclude=bigdevops/node_modules bigdevops
+tar zvcf bigdevops_frontend_`date +%Y-%m-%d_%H-%M-%S`.tar.gz --exclude=bigdevops/node_modules ../bigdevops
